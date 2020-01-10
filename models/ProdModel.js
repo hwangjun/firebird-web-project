5c4ff7dbda2a8c8ed0440d49b3302da88d467ded
kong8715 <kong8715@gmail.com>
2020-01-11 08:04:49 +0900

    product, env

--- models/ProdModel.js
+++ models/ProdModel.js
@@ -37,9 +37,21 @@ const prodSchema = mongoose.Schema({
     },
 });
 
-prodSchema.statics.addProd = function (prod) {
+prodSchema.statics.getProduct = function (prod_c) {
+    return this.find({prod_c: prod_c});
+}
+
+prodSchema.statics.getProductList = function (query, skip, limit) {
+    return this.find(query).skip(skip).limit(limit);
+}
+
+prodSchema.statics.addProduct = function (prod) {
     const newProd = new this(prod);
     return newProd.save();
+}
+
+prodSchema.statics.deleteProduct = function (prod_c) {
+    return this.deleteOne({prod_c: prod_c});
 }
 
 module.exports = mongoose.model('Prod', prodSchema, 'TB_PROD');
--- routes/api/prod.js
+++ routes/api/prod.js
@@ -1,34 +1,141 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator');
 const Prod = require('../../models/ProdModel');
 
-
-router.get('/', async (req, res) => {
-    console.log('/prod Call');
-    let prodList = await Prod.find({prod_c: 'aaaaa'});
-    console.log(prodList);
-    res.json(prodList);
-});
-
-router.get('/add', async (req, res, next) => {
-    console.log('/add Call1');
-    let result = {
+/**
+ * 상품 조회
+ */
+router.get('/:prod_c', check('prod_c').isNumeric(),(req, res) => {
+    let responseResult = {
         state: 200,
         desc: '',
-        data: null
+        result: null
     }
+
+    const validationError = validationResult(req);
+    // 유효성 검사 통과 못할 경우 false
+    if (false == validationError.isEmpty()) {
+        responseResult.state = 400;
+        responseResult.desc = validationError.errors[0].msg + '[' + validationError.errors[0].param + ']';
+    }
+    if (400 != responseResult.state) {
+        Prod.getProd(req.params.prod_c)
+            .then((result) => {
+                responseResult.result = result;
+            })
+            .catch((err) => {
+                responseResult.result = err;
+                responseResult.state = 500;
+            })
+            .then(()=>{
+                res.status(responseResult.state).json(responseResult);
+            });
+    }
+});
+
+/**
+ * 상품 리스트 조회
+ */
+router.get('/', (req, res) => {
+    let responseResult = {
+        state: 200,
+        desc: '',
+        result: null
+    }
+
+    let prod_n = req.query.prod_n != null ? req.query.prod_n.trim() : null;
+    let skip = req.query.offset != null ? Number(req.query.offset) : 0;
+    let limit = req.query.limit != null ? Number(req.query.limit) : 10;
     
-    let newProd = {
-        prod_c: req.query.prod_c,
-        prod_n: req.query.prod_n
+    let query = {};
+    if (null != prod_n && '' != prod_n) {
+        query.prod_n = prod_n
+    }
+
+    Prod.getProductList(query, skip, limit)
+        .then((r) => {
+            responseResult.result = r;
+        })
+        .catch((err) => {
+            responseResult.state = 500;
+            responseResult.desc = 'productList Search FAIL';
+        })
+        .then(()=>{
+            res.status(responseResult.state).json(responseResult);
+        });
+});
+
+/**
+ * 상품 추가
+ */
+router.post('/', [check('prod_c').isInt(), check('prod_n').isString()]
+            , (req, res, next) => {
+    let responseResult = {
+        state: 200,
+        desc: '',
+        result: null
+    }
+
+    const validationError = validationResult(req);
+    // 유효성 검사 통과 못할 경우 false
+    if (false == validationError.isEmpty()) {
+        responseResult.state = 400;
+        responseResult.desc = validationError.errors[0].msg + '[' + validationError.errors[0].param + ']';
+        res.status(responseResult.state).json(responseResult);
+    }
+
+    if (400 != responseResult.state) {
+        let newProd = {
+            prod_c: req.query.prod_c,
+            prod_n: req.query.prod_n
+        };
+
+        Prod.addProduct(newProd).then((r) => {
+            responseResult.result = r;
+        }).catch((err) => {
+            console.error('product Add Error : ', err);
+            responseResult.state = 500;
+            responseResult.desc = 'product Add FAIL';
+        })
+        .then(()=>{
+            res.status(responseResult.state).json(responseResult);
+        });
+    }
+});
+
+/**
+ * 상품 삭제
+ */
+router.delete('/:prod_c', check('prod_c').isInt(), (req, res) => {
+    let responseResult = {
+        state: 200,
+        desc: '',
+        result: null
+    }
+
+    const validationError = validationResult(req);
+     // 유효성 검사 통과 못할 경우 false
+    if (false == validationError.isEmpty()) {
+        console.log('validator fasle :' , validationError);
+        responseResult.state = 400;
+        responseResult.desc = validationError.errors[0].msg + '[' + validationError.errors[0].param + ']';
+        res.status(responseResult.state).json(responseResult);
     };
 
-    let addResult = await Prod.addProd(newProd);
-    result['data'] = addResult;
-    console.log('result : ', result);
-    console.log('/add Call2');
-    
-    res.status(201).json(result);
-});
+    if (400 != responseResult.state) {
+    Prod.deleteProduct(req.params.prod_c)
+        .then((r) => {
+            responseResult.result = r;
+        })
+        .catch((err) => {
+            responseResult.state = 500;
+            responseResult.desc = 'product DELETE FAIL';
+        })
+        .then(() => {
+            res.status(responseResult.state).json(responseResult);
+        })
+    }
+})
 
 module.exports = router;
