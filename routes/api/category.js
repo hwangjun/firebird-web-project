de24cc5349984f7675f2c62956b0d0cedfbdce7f
hwangjun <hwangjunses@naver.com>
2020-02-01 11:02:03 +0900

    api partner  > partners update

--- routes/api/category.js
+++ routes/api/category.js
@@ -3,7 +3,7 @@
 import express from "express";
 const router = express.Router();
 import Category from "../../models/product/Category";
-import {isLoggedin, checkAdminPermission} from "../../config/authMiddleware";
+import { isLoggedin, checkAdminPermission } from "../../config/authMiddleware";
 
 /**
  * @swagger
@@ -15,11 +15,13 @@ import {isLoggedin, checkAdminPermission} from "../../config/authMiddleware";
  *       200:
  *         description: 성공
  */
-router.get('/', (req, res) => {
-    Category.findAll().then((category) => {
-        if (!category.length) return res.status(404).send({ err: 'Category not found' });
-        // res.send(`find successfully: ${category}`);
-        res.send(category);
+router.get("/", (req, res) => {
+  Category.findAll()
+    .then(category => {
+      if (!category.length)
+        return res.status(404).send({ err: "Category not found" });
+      // res.send(`find successfully: ${category}`);
+      res.send(category);
     })
     .catch(err => res.status(500).send(err));
 });
@@ -41,107 +43,107 @@ router.get('/', (req, res) => {
  *       200:
  *         description: 성공
  */
-router.get('/:categoryCode', isLoggedin , checkAdminPermission, (req, res) => {
-    Category.findByCategoryCode(req.params.categoryCode).then((category) => {
-      if (!category) return res.status(404).send({ err: 'Category not found' });
-        // res.send(`findByCategoryCode successfully: ${category}`);
-        res.send(category);
+router.get("/:categoryCode", isLoggedin, checkAdminPermission, (req, res) => {
+  Category.findByCategoryCode(req.params.categoryCode)
+    .then(category => {
+      if (!category) return res.status(404).send({ err: "Category not found" });
+      // res.send(`findByCategoryCode successfully: ${category}`);
+      res.send(category);
     })
     .catch(err => res.status(500).send(err));
 });
 
 /**
-*    @swagger
-*    /api/categorys:
-*    post:
-*      summary: 카테고리 생성
-*      tags: [Category]
-*      consumes:
-*        - application/x-www-form-urlencoded
-*      parameters:
-*        - in: formData
-*          name: categoryCode
-*          type: string
-*          description: 카테고리 코드
-*        - in: formData
-*          name: parentCategoryCode
-*          type: string
-*          description: 부모 카테고리 코드
-*        - in: formData
-*          name: categoryName
-*          type: string
-*          description: 카레고리명
-*        - in: formData
-*          name: depth
-*          type: string
-*          description: 깊이
-*      responses:
-*        200:
-*          description: OK
-*/
-router.post('/', isLoggedin , checkAdminPermission, (req, res) => {
-    Category.create(req.body)
-        .then(category => res.send(category))
-        .catch(err => res.status(500).send(err));
-});
-
-
-/**
-*    @swagger
-*    /api/categorys:
-*    patch:
-*      summary: 카테고리 수정
-*      tags: [Category]
-*      consumes:
-*        - application/x-www-form-urlencoded
-*      parameters:
-*        - in: formData
-*          name: categoryCode
-*          type: string
-*          description: 카테고리 코드
-*        - in: formData
-*          name: parentCategoryCode
-*          type: string
-*          description: 부모 카테고리 코드
-*        - in: formData
-*          name: categoryName
-*          type: string
-*          description: 카레고리명
-*        - in: formData
-*          name: depth
-*          type: string
-*          description: 깊이
-*      responses:
-*        200:
-*          description: OK
-*/
-router.patch('/', isLoggedin , checkAdminPermission, (req, res) => {
-    Category.update(req.body)
-        .then(category => res.send(category))
-        .catch(err => res.status(500).send(err));
+ *    @swagger
+ *    /api/categorys:
+ *    post:
+ *      summary: 카테고리 생성
+ *      tags: [Category]
+ *      consumes:
+ *        - application/x-www-form-urlencoded
+ *      parameters:
+ *        - in: formData
+ *          name: categoryCode
+ *          type: string
+ *          description: 카테고리 코드
+ *        - in: formData
+ *          name: parentCategoryCode
+ *          type: string
+ *          description: 부모 카테고리 코드
+ *        - in: formData
+ *          name: categoryName
+ *          type: string
+ *          description: 카레고리명
+ *        - in: formData
+ *          name: depth
+ *          type: string
+ *          description: 깊이
+ *      responses:
+ *        200:
+ *          description: OK
+ */
+router.post("/", isLoggedin, checkAdminPermission, (req, res) => {
+  Category.create(req.body)
+    .then(category => res.send(category))
+    .catch(err => res.status(500).send(err));
 });
 
 /**
-*    @swagger
-*    /api/categorys:
-*    delete:
-*      summary: 카테고리 제거
-*      tags: [Category]
-*      consumes:
-*        - application/x-www-form-urlencoded
-*      parameters:
-*        - in: formData
-*          name: categoryCode
-*          type: string
-*          description: 카테고리 코드
-*      responses:
-*        200:
-*          description: OK
-*/
-router.delete('/', isLoggedin , checkAdminPermission, (req, res) => {
-    Category.delete(req.body.categoryCode)
-        .then(category => res.send(category))
-        .catch(err => res.status(500).send(err));
+ *    @swagger
+ *    /api/categorys:
+ *    patch:
+ *      summary: 카테고리 수정
+ *      tags: [Category]
+ *      consumes:
+ *        - application/x-www-form-urlencoded
+ *      parameters:
+ *        - in: formData
+ *          name: categoryCode
+ *          type: string
+ *          description: 카테고리 코드
+ *        - in: formData
+ *          name: parentCategoryCode
+ *          type: string
+ *          description: 부모 카테고리 코드
+ *        - in: formData
+ *          name: categoryName
+ *          type: string
+ *          description: 카레고리명
+ *        - in: formData
+ *          name: depth
+ *          type: string
+ *          description: 깊이
+ *      responses:
+ *        200:
+ *          description: OK
+ */
+router.patch("/", isLoggedin, checkAdminPermission, (req, res) => {
+  Category.update(req.body)
+    .then(category => res.send(category))
+    .catch(err => res.status(500).send(err));
+});
+
+/**
+ *    @swagger
+ *    /api/categorys:
+ *    delete:
+ *      summary: 카테고리 제거
+ *      tags: [Category]
+ *      consumes:
+ *        - application/x-www-form-urlencoded
+ *      parameters:
+ *        - in: formData
+ *          name: categoryCode
+ *          type: string
+ *          description: 카테고리 코드
+ *      responses:
+ *        200:
+ *          description: OK
+ */
+router.delete("/", isLoggedin, checkAdminPermission, (req, res) => {
+  Category.delete(req.body.categoryCode)
+    .then(category => res.send(category))
+    .catch(err => res.status(500).send(err));
 });
 
 // module.exports = router;
--- routes/api/partner.js
+++ routes/api/partner.js
@@ -2,10 +2,11 @@
 // const Partner = require('../../models/product/Partner');
 import express from "express";
 const router = express.Router();
+import Partner from "../../models/product/Partner";
 
 /**
  * @swagger
- * /api/partner:
+ * /api/partners:
  *   get:
  *     summary: 모든 협력사 조회
  *     tags: [Partner]
@@ -13,17 +14,19 @@ const router = express.Router();
  *       200:
  *         description: 성공
  */
-router.get('/', (req, res) => {
-    Partner.findAll().then((partner) => {
-        if (!partner.length) return res.status(404).send({ err: 'Partner not found' });
-        res.send(partner);
+router.get("/", (req, res) => {
+  Partner.findAll()
+    .then(partner => {
+      if (!partner.length)
+        return res.status(404).send({ err: "Partner not found" });
+      res.send(partner);
     })
     .catch(err => res.status(500).send(err));
 });
 
 /**
  * @swagger
- * /api/partner/{partnerCode}:
+ * /api/partners/{partnerCode}:
  *   get:
  *     summary: 특정 협력사 조회
  *     tags: [Partner]
@@ -38,97 +41,98 @@ router.get('/', (req, res) => {
  *       200:
  *         description: 성공
  */
-router.get('/:partnerCode', (req, res) => {
-    Partner.findByPartnerCode(req.params.partnerCode).then((partner) => {
-      if (!partner) return res.status(404).send({ err: 'Partner not found' });
-        res.send(partner);
+router.get("/:partnerCode", (req, res) => {
+  Partner.findByPartnerCode(req.params.partnerCode)
+    .then(partner => {
+      if (!partner) return res.status(404).send({ err: "Partner not found" });
+      res.send(partner);
     })
     .catch(err => res.status(500).send(err));
 });
 
 /**
-*    @swagger
-*    /api/partner:
-*    post:
-*      summary: 협력사 생성
-*      tags: [Partner]
-*      consumes:
-*        - application/x-www-form-urlencoded
-*      parameters:
-*        - in: formData
-*          name: partnerCode
-*          type: string
-*          description: 협력사 코드
-*        - in: formData
-*          name: partnerName
-*          type: string
-*          description: 협력사명
-*        - in: formData
-*          name: companyName
-*          type: string
-*          description: 회사명
-*      responses:
-*        200:
-*          description: OK
-*/
-router.post('/', (req, res) => {
-    Partner.create(req.body)
-        .then(partner => res.send(partner))
-        .catch(err => res.status(500).send(err));
+ *    @swagger
+ *    /api/partners:
+ *    post:
+ *      summary: 협력사 생성
+ *      tags: [Partner]
+ *      consumes:
+ *        - application/x-www-form-urlencoded
+ *      parameters:
+ *        - in: formData
+ *          name: partnerCode
+ *          type: string
+ *          description: 협력사 코드
+ *        - in: formData
+ *          name: partnerName
+ *          type: string
+ *          description: 협력사명
+ *        - in: formData
+ *          name: companyName
+ *          type: string
+ *          description: 회사명
+ *      responses:
+ *        200:
+ *          description: OK
+ */
+router.post("/", (req, res) => {
+  Partner.create(req.body)
+    .then(partner => res.send(partner))
+    .catch(err => res.status(500).send(err));
 });
 
 /**
-*    @swagger
-*    /api/partner:
-*    patch:
-*      summary: 협력사 수정
-*      tags: [Partner]
-*      consumes:
-*        - application/x-www-form-urlencoded
-*      parameters:
-*        - in: formData
-*          name: partnerCode
-*          type: string
-*          description: 협력사 코드
-*        - in: formData
-*          name: partnerName
-*          type: string
-*          description: 협력사명
-*        - in: formData
-*          name: companyName
-*          type: string
-*          description: 회사명
-*      responses:
-*        200:
-*          description: OK
-*/
-router.patch('/', (req, res) => {
-    Partner.update(req.body)
-        .then(partner => res.send(partner))
-        .catch(err => res.status(500).send(err));
+ *    @swagger
+ *    /api/partners:
+ *    patch:
+ *      summary: 협력사 수정
+ *      tags: [Partner]
+ *      consumes:
+ *        - application/x-www-form-urlencoded
+ *      parameters:
+ *        - in: formData
+ *          name: partnerCode
+ *          type: string
+ *          description: 협력사 코드
+ *        - in: formData
+ *          name: partnerName
+ *          type: string
+ *          description: 협력사명
+ *        - in: formData
+ *          name: companyName
+ *          type: string
+ *          description: 회사명
+ *      responses:
+ *        200:
+ *          description: OK
+ */
+router.patch("/", (req, res) => {
+  Partner.update(req.body)
+    .then(partner => res.send(partner))
+    .catch(err => res.status(500).send(err));
 });
 
 /**
-*    @swagger
-*    /api/partner:
-*    delete:
-*      summary: 협력사 제거
-*      tags: [Partner]
-*      consumes:
-*        - application/x-www-form-urlencoded
-*      parameters:
-*        - in: formData
-*          name: partnerCode
-*          type: string
-*          description: 협력사 코드
-*      responses:
-*        200:
-*          description: OK
-*/
-router.delete('/', (req, res) => {
-    Partner.delete(req.body.partnerCode)
-        .then(partnerCode => res.send(partnerCode))
-        .catch(err => res.status(500).send(err));
+ *    @swagger
+ *    /api/partners:
+ *    delete:
+ *      summary: 협력사 제거
+ *      tags: [Partner]
+ *      consumes:
+ *        - application/x-www-form-urlencoded
+ *      parameters:
+ *        - in: formData
+ *          name: partnerCode
+ *          type: string
+ *          description: 협력사 코드
+ *      responses:
+ *        200:
+ *          description: OK
+ */
+router.delete("/", (req, res) => {
+  Partner.delete(req.body.partnerCode)
+    .then(partnerCode => res.send(partnerCode))
+    .catch(err => res.status(500).send(err));
 });
 
 // module.exports = router;
